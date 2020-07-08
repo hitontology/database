e39068074a307b0721f4d7dfe94d15951a350f9a
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-07-08 17:06:58 +0200

    Create delete and insert statements for external classes and instances.

--- scripts/dbpedia/download.py
+++ scripts/dbpedia/download.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import os
 
 language = {
     "query": '''SELECT REPLACE(STR(?uri),"http://dbpedia.org/resource/","") as ?suffix STR(SAMPLE(?label)) AS ?label
@@ -10,11 +11,12 @@ language = {
  FILTER(LANGMATCHES(LANG(?label),"en"))
 }''',
     "endpoint": "https://dbpedia.org/sparql",
+    "table": "language"
 }
 
 #  SWO is uploaded to the HITO endpoint, they are (transitive) subclasses, not instances
 license = {
-    "query": '''PREFIX swo: <http://www.ebi.ac.uk/swo/license/>
+    "query": '''PREFIX swo: <http://www.ebi.ac.uk/swo/>
 SELECT REPLACE(STR(?uri),"http://www.ebi.ac.uk/swo/","") as ?suffix STR(SAMPLE(?label)) AS ?label
 FROM <http://www.ebi.ac.uk/swo/swo.owl/1.7>
 {
@@ -22,7 +24,8 @@ FROM <http://www.ebi.ac.uk/swo/swo.owl/1.7>
       rdfs:label ?label.
  FILTER(LANGMATCHES(LANG(?label),"en"))
 }''',
-    "endpoint": "https://hitontology.eu/sparql"
+    "endpoint": "https://hitontology.eu/sparql",
+    "table": "license"
 }
 
 programmingLanguage = {
@@ -32,13 +35,32 @@ programmingLanguage = {
       rdfs:label ?label.
  FILTER(LANGMATCHES(LANG(?label),"en"))
 }''',
-    "endpoint": "https://dbpedia.org/sparql"
+    "endpoint": "https://dbpedia.org/sparql",
+    "table": "programmingLanguage"
 }
 
-classes = [programmingLanguage]
+classes = [language,license,programmingLanguage]
 
 for clazz in classes:
-    parameters = {"query": clazz["query"], "format": "text/comma-separated-values"}
+    filename=clazz['table']+".sql"
+    output=open(filename, "w")
+    output.write("DELETE FROM "+clazz['table']+";\n")
+    output.write("INSERT INTO "+clazz['table']+"(suffix,label) VALUES"+'\n')
+    parameters = {"query": clazz["query"], "format": "text/tab-separated-values"}
     resp = requests.get(clazz["endpoint"],params=parameters)
-    tsv = resp.text
+    readCSV = csv.reader(resp.text.splitlines(), delimiter='\t')
+    next(readCSV, None)
+    print(resp.text)
+    for line in readCSV:
+        output.write('(E\''+line[0].replace("'","\\'")+'\',E\''+line[1].replace("'","\\'")+'\'),\n')
+#        print(line)
+#        pass
+#        print('(\''+line[0]+'\',\''+line[1])
      
+    output.close()
+    #truncate last char of the file and replace it with ;
+    with open(filename, 'rb+') as filehandle:
+        filehandle.seek(-2, os.SEEK_END)
+        filehandle.truncate()
+    with open (filename, "a+") as append:
+        append.write(';')
