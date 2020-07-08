d70b289386a0b8c147716191b387605e8a66189e
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-07-08 11:12:48 +0200

    Add generated columns.

--- scripts/hitotables.sql
+++ scripts/hitotables.sql
@@ -7,7 +7,7 @@ create type CatalogueType as enum('UserGroup', 'ApplicationSystem', 'Feature', '
 -- client and databasesystem as arrays because they should be [0..n]
 -- inserting new values needs to be like 'VALUES('{"bla","blubb"}')'
 create table softwareproduct(
-	uri VARCHAR(200) PRIMARY KEY,
+	suffix VARCHAR(200) PRIMARY KEY,
 	label VARCHAR(200) NOT NULL,
 	comment text,
 	codeRepository VARCHAR(200),
@@ -15,33 +15,35 @@ create table softwareproduct(
 	client Client [],
 	databaseSystem DatabaseSystem []
 );
+
 -- the atomic tables mostly filled with dbpedia data
 create table programmingLibrary(
-	uri VARCHAR(200) PRIMARY KEY,
-	label VARCHAR(200) NOT NULL
+	suffix VARCHAR(200) PRIMARY KEY,
+	label VARCHAR(200) NOT NULL,
+	uri VARCHAR(229) GENERATED ALWAYS AS ('http://dbpedia.org/resource/' || suffix) STORED
 );
 create table language(
-	uri VARCHAR(200) PRIMARY KEY,
+	suffix VARCHAR(200) PRIMARY KEY,
 	label VARCHAR(200) NOT NULL
 );
 create table interoperabilityStandard(
-	uri VARCHAR(200) PRIMARY KEY,
+	suffix VARCHAR(200) PRIMARY KEY,
 	label VARCHAR(200) NOT NULL
 );
 create table programmingLanguage(
-	uri VARCHAR(200) PRIMARY KEY,
+	suffix VARCHAR(200) PRIMARY KEY,
 	label VARCHAR(200) NOT NULL
 );
 create table operatingSystem(
-	uri VARCHAR(200) PRIMARY KEY,
+	suffix VARCHAR(200) PRIMARY KEY,
 	label VARCHAR(200) NOT NULL
 );
 create table license(
-	uri VARCHAR(200) PRIMARY KEY,
+	suffix VARCHAR(200) PRIMARY KEY,
 	label VARCHAR(200) NOT NULL
 );
 create table component(
-	uri VARCHAR(200) PRIMARY KEY,
+	suffix VARCHAR(200) PRIMARY KEY,
 	label VARCHAR(200) NOT NULL
 );
 -- HITO catalogues as one table with type attribute
@@ -58,44 +60,44 @@ create table classified(
 create table citation(
 	suffix VARCHAR(200) PRIMARY KEY,
 	label VARCHAR(200) NOT NULL,
-	swp_uri character varying(200) NOT NULL REFERENCES softwareproduct(uri) ON DELETE CASCADE ON UPDATE CASCADE,
+	swp_suffix character varying(200) NOT NULL REFERENCES softwareproduct(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
 	classified_suffix VARCHAR(200) NOT NULL REFERENCES classified(suffix) ON DELETE CASCADE ON UPDATE CASCADE
 );
 -- relations from atomics to master
 create table swp_has_programmingLibrary(
-	swp_uri character varying(200) NOT NULL REFERENCES softwareproduct(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	lib_uri character varying(200) NOT NULL	REFERENCES programmingLibrary(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY (swp_uri, lib_uri)
+	swp_suffix character varying(200) NOT NULL REFERENCES softwareproduct(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	lib_suffix character varying(200) NOT NULL	REFERENCES programmingLibrary(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (swp_suffix, lib_suffix)
 );
 create table swp_has_language(
-	swp_uri character varying(200) NOT NULL REFERENCES softwareproduct(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	lang_uri character varying(200) NOT NULL	REFERENCES language(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY (swp_uri, lang_uri)
+	swp_suffix character varying(200) NOT NULL REFERENCES softwareproduct(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	lang_suffix character varying(200) NOT NULL	REFERENCES language(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (swp_suffix, lang_suffix)
 );
 create table swp_has_interoperabilityStandard(
-	swp_uri character varying(200) NOT NULL REFERENCES softwareproduct(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	io_uri character varying(200) NOT NULL	REFERENCES interoperabilityStandard(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY (swp_uri, io_uri)
+	swp_suffix character varying(200) NOT NULL REFERENCES softwareproduct(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	io_suffix character varying(200) NOT NULL	REFERENCES interoperabilityStandard(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (swp_suffix, io_suffix)
 );
 create table swp_has_programmingLanguage(
-	swp_uri character varying(200) NOT NULL REFERENCES softwareproduct(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	plang_uri character varying(200) NOT NULL	REFERENCES programmingLanguage(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY (swp_uri, plang_uri)
+	swp_suffix character varying(200) NOT NULL REFERENCES softwareproduct(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	plang_suffix character varying(200) NOT NULL	REFERENCES programmingLanguage(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (swp_suffix, plang_suffix)
 );
 create table swp_has_operatingSystem(
-	swp_uri character varying(200) NOT NULL REFERENCES softwareproduct(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	os_uri character varying(200) NOT NULL	REFERENCES operatingSystem(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY (swp_uri, os_uri)
+	swp_suffix character varying(200) NOT NULL REFERENCES softwareproduct(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	os_suffix character varying(200) NOT NULL	REFERENCES operatingSystem(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (swp_suffix, os_suffix)
 );
 create table swp_has_license(
-	swp_uri character varying(200) NOT NULL REFERENCES softwareproduct(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	license_uri character varying(200) NOT NULL	REFERENCES license(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY (swp_uri, license_uri)
+	swp_suffix character varying(200) NOT NULL REFERENCES softwareproduct(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	license_suffix character varying(200) NOT NULL	REFERENCES license(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (swp_suffix, license_suffix)
 );
 -- ToDo: how to avoid circular reference?
 create table swp_has_component(
-	swp_uri character varying(200) NOT NULL REFERENCES softwareproduct(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	component_uri character varying(200) NOT NULL REFERENCES softwareproduct(uri) ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY (swp_uri, component_uri)
+	swp_suffix character varying(200) NOT NULL REFERENCES softwareproduct(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	component_suffix character varying(200) NOT NULL REFERENCES softwareproduct(suffix) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (swp_suffix, component_suffix)
 );
 
