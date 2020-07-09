ffb7a321bfdc31fa179296d83c6edb544cb57a49
ThomasPause <tp55sude@studserv.uni-leipzig.de>
2020-07-09 14:07:19 +0200

    write output into sql file

--- scripts/classifiedparser.py
+++ scripts/classifiedparser.py
@@ -1,5 +1,6 @@
 import pandas
 import os
+
 filenames={
 #    "applicationsystem.csv":"ApplicationSystem",
     "function.csv":"Function",
@@ -17,33 +18,31 @@ for folder in folders:
             catalogueType=filenames[filename]
             catalogue_suffix=folder+catalogueType+"Catalogue"
             outputfile=catalogueType
-            output=open(folder+"/"+catalogueType+".sql", "a+")
+            output=open(folder+"/"+catalogueType+".sql", "w")
             output.write("INSERT INTO classified(suffix,n,label,comment,synonyms,catalogue_suffix) VALUES"+'\n')
 
             df = pandas.read_csv(inPath)
             print(df)
             for index, row in df.iterrows():
-               
+
                 quotedN = "NULL"
                 if "n" in row:
                     quotedN = "'"+str(row["n"])+"'"
-                
+
                 quotedComment = "NULL"
                 if "comment" in row:
                     quotedComment = "'"+row["comment"]+"'"
                 synonyms = []
                 if "synonyms" in row:
                     synonyms = row["synonyms"].split(";")
-                
-                synonymString = ",".join(map(lambda x: '"'+x+'"', synonyms)) 
 
-                print(f"('{row['uri']}',{quotedN},'{row['en']}',{quotedComment},'{{{synonymString}}}','{catalogue_suffix}'),\n")
-#                output.write( '(\''+line[1]+'\',\''+line[0]+'\',\''+catalogue_suffix+'\'),\n')
-            #     #print(line)
-            # output.close()
-            # #truncate last char of the file and replace it with ;
-            # with open(folder+"/"+catalogueType+".sql", 'rb+') as filehandle:
-            #     filehandle.seek(-2, os.SEEK_END)
-            #     filehandle.truncate()
-            # with open (folder+"/"+catalogueType+".sql", "a+") as append:
-            #     append.write(';')
+                synonymString = ",".join(map(lambda x: '"'+x+'"', synonyms))
+
+                output.write(f"('{row['uri']}',{quotedN},'{row['en']}',{quotedComment},'{{{synonymString}}}','{catalogue_suffix}'),\n")
+            output.close()
+                #truncate last char of the file and replace it with ;
+            with open(folder+"/"+catalogueType+".sql", 'rb+') as filehandle:
+                filehandle.seek(-2, os.SEEK_END)
+                filehandle.truncate()
+            with open (folder+"/"+catalogueType+".sql", "a+") as append:
+                append.write(';')
