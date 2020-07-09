0421f758567ff3a204b65ed930808a5d4a5bb9d3
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-07-09 13:14:53 +0200

    Switch to pandas.

--- scripts/classifiedparser.py
+++ scripts/classifiedparser.py
@@ -1,34 +1,49 @@
-import pandas as pd
+import pandas
 import os
 filenames={
-    "applicationsystem.csv":"ApplicationSystem",
+#    "applicationsystem.csv":"ApplicationSystem",
     "function.csv":"Function",
-    "subfeature.csv":"SubFeature",
+#    "subfeature.csv":"SubFeature",
 }
-folders=["smörebröd"]
 #folders=["BbApplicationComponent","BbArchitecture","BbReferenceModel","WhoDhiClient","WhoDhiDataService","WhoDhiHealthcareProvider","WhoDhiHealthSystemManager","WhoDhiSystemCategory"]
+folders=["BbArchitecture"]
 #filenames=["applicationsystem.csv","function.csv"]
+#filenames=["function.csv"]
 for folder in folders:
     for filename in filenames:
-        if(os.path.isfile(folder+"/"+filename)):
+        inPath = folder+"/"+filename;
+        if(os.path.isfile(inPath)):
             #order: foldername + csv-Name with CamelCase is the catalogue_suffix
-            shortFile=filenames[filename]
-            catalogue_suffix=folder+shortFile+"Catalogue"
-            outputfile=shortFile
-            output=open(folder+"/"+shortFile+".sql", "a+")
-            output.write("INSERT INTO classified(suffix,label,catalogue_suffix) VALUES"+'\n')
+            catalogueType=filenames[filename]
+            catalogue_suffix=folder+catalogueType+"Catalogue"
+            outputfile=catalogueType
+            output=open(folder+"/"+catalogueType+".sql", "a+")
+            output.write("INSERT INTO classified(suffix,n,label,comment,synonyms,catalogue_suffix) VALUES"+'\n')
 
-            for df in pd.read_csv(folder+"/"+filename, sep=',',chunksize=1):
-                print(df)
-            #readCSV = pd.read_csv(folder+"/"+filename, sep=',',usecols=["en","uri"])
-            #next(readCSV, None)
-            # for line in readCSV:
-            #     output.write('(\''+line[1]+'\',\''+line[0]+'\',\''+catalogue_suffix+'\'),\n')
+            df = pandas.read_csv(inPath)
+            print(df)
+            for index, row in df.iterrows():
+               
+                quotedN = "NULL"
+                if "n" in row:
+                    quotedN = "'"+str(row["n"])+"'"
+                
+                quotedComment = "NULL"
+                if "comment" in row:
+                    quotedComment = "'"+row["comment"]+"'"
+                synonyms = []
+                if "synonyms" in row:
+                    synonyms = row["synonyms"].split(";")
+                
+                synonymString = ",".join(map(lambda x: '"'+x+'"', synonyms)) 
+
+                print(f"('{row['uri']}',{quotedN},'{row['en']}',{quotedComment},'{{{synonymString}}}','{catalogue_suffix}'),\n")
+#                output.write( '(\''+line[1]+'\',\''+line[0]+'\',\''+catalogue_suffix+'\'),\n')
             #     #print(line)
             # output.close()
             # #truncate last char of the file and replace it with ;
-            # with open(folder+"/"+shortFile+".sql", 'rb+') as filehandle:
+            # with open(folder+"/"+catalogueType+".sql", 'rb+') as filehandle:
             #     filehandle.seek(-2, os.SEEK_END)
             #     filehandle.truncate()
-            # with open (folder+"/"+shortFile+".sql", "a+") as append:
+            # with open (folder+"/"+catalogueType+".sql", "a+") as append:
             #     append.write(';')
