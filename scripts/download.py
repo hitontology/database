b4b275365f57a565db4474145c133a6b66d81dac
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-07-16 13:49:41 +0200

    Download DBpedia along with interoperability standards.

--- scripts/download.py
+++ scripts/download.py
@@ -2,6 +2,21 @@ import requests
 import csv
 import os
 
+standard = {
+    "query": '''SELECT REPLACE(STR(?uri),"http://hitontology.eu/ontology/","") as ?suffix
+        STR(SAMPLE(?label)) AS ?label
+        STR(SAMPLE(?comment)) AS ?comment
+        GROUP_CONCAT(?source;separator="'|'") AS ?sources
+ {
+  ?uri a hito:Interoperability;
+          rdfs:label ?label;
+          rdfs:comment ?z;
+          <http://purl.org/dc/terms/source> ?source.
+}''',
+    "endpoint": "https://hitontology.eu/sparql",
+    "table": "interoperabilityStandards"
+}
+
 language = {
     "query": '''SELECT REPLACE(STR(?uri),"http://dbpedia.org/resource/","") as ?suffix STR(SAMPLE(?label)) AS ?label
 {
@@ -18,7 +33,7 @@ language = {
 license = {
     "query": '''PREFIX swo: <http://www.ebi.ac.uk/swo/>
 SELECT REPLACE(STR(?uri),"http://www.ebi.ac.uk/swo/","") as ?suffix STR(SAMPLE(?label)) AS ?label
-FROM <http://www.ebi.ac.uk/swo/swo.owl/1.7>
+#FROM <http://www.ebi.ac.uk/swo/swo.owl/1.7>
 {
  ?uri rdfs:subClassOf+ swo:SWO_0000002;
       rdfs:label ?label.
@@ -39,28 +54,33 @@ programmingLanguage = {
     "table": "programmingLanguage"
 }
 
-classes = [language,license,programmingLanguage]
+
+def valueMap(value,isArray):
+    if(isArray):
+        return "{" + ",".join(map(lambda v: valueMap(v,False), value.split("|")))  + "}"
+    if(value==''):
+        return 'NULL'
+    return "E'"+value.replace("'","\\'")+"'" # escape single quotes, add quotes for SQL
+
+def insert(values):
+    mapped = []
+    for i in range(len(values)):
+        mapped.append(valueMap(values[i],i==3)) # there can be more than one source, it is at position 3 counting from 0 
+    s = ",".join(mapped) 
+    return "("+s+")"
+
+classes = [standard,language,license,programmingLanguage]
 
 for clazz in classes:
     filename=clazz['table']+".sql"
-    output=open(filename, "w")
+    output=open("output/"+filename, "w")
     output.write("DELETE FROM "+clazz['table']+";\n")
     output.write("INSERT INTO "+clazz['table']+"(suffix,label) VALUES"+'\n')
     parameters = {"query": clazz["query"], "format": "text/tab-separated-values"}
     resp = requests.get(clazz["endpoint"],params=parameters)
     readCSV = csv.reader(resp.text.splitlines(), delimiter='\t')
-    next(readCSV, None)
-    print(resp.text)
-    for line in readCSV:
-        output.write('(E\''+line[0].replace("'","\\'")+'\',E\''+line[1].replace("'","\\'")+'\'),\n')
-#        print(line)
-#        pass
-#        print('(\''+line[0]+'\',\''+line[1])
-     
+    next(readCSV, None) # skip CSV header
+    content = ",\n".join(map(lambda line: insert(line), readCSV))
+    output.write(content)
+    output.write(";")
     output.close()
-    #truncate last char of the file and replace it with ;
-    with open(filename, 'rb+') as filehandle:
-        filehandle.seek(-2, os.SEEK_END)
-        filehandle.truncate()
-    with open (filename, "a+") as append:
-        append.write(';')
